25a4a28942005e6ef277a222691b180b2f76cf0f
Héctor Aguilar <hebodio@gmail.com>
2021-07-12 14:57:13 -0400

    [FIX] Ajusta manifest

--- skrt_sale/__manifest__.py
+++ skrt_sale/__manifest__.py
@@ -1,22 +1,21 @@
 # Copyright 2021, Héctor Aguilar
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    'name': 'Base extension Addons for skyalert',
+    'name': 'Sales extension Addons for skyalert',
     'summary': 'Module summary',
-    'version': '0.0.1',
-    'category': 'SkyAlert',
-    'website': '',
-    'author': '<Héctor Aguilar>, Ideanet S.A de C.V',
+    'version': '0.1',
+    'category': 'Sales',
+    'website': 'https://odoo-community.org/',
+    'author': '<Héctor Aguilar>, Odoo Community Association (OCA)',
     'license': 'LGPL-3',
     'application': False,
     'installable': True,
-    'external_dependencies': {
-    },
     'depends': [
         'base',
+        'sale',
+        'sale_crm',
     ],
     'data': [
-        'views/res_partner_view.xml'
     ],
     'demo': [
     ],
